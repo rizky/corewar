f20ce66215fb396a85e4bc5d6bfd255da7228068
Rizky Ario <rizkyario@yahoo.com>
2018-03-21 23:58:30 +0100

    Added 12 bytes after .comment

--- src/ft_asm.c
+++ src/ft_asm.c
@@ -6,7 +6,7 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 14:40:47 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/03/21 22:30:31 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/03/21 23:53:27 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,21 @@ void
 }
 
 void
-	cw_append_name(t_array *binary, char *str)
+	cw_append_magic_nbr(t_array *binary)
 {
-	int i;
-
 	fta_append_char(binary, 0x00);
 	fta_append_char(binary, 0xea);
 	fta_append_char(binary, 0x83);
 	fta_append_char(binary, 0xf3);
+}
+
+void
+	cw_append_name(t_array *binary, char *str)
+{
+	int i;
+
 	fta_append(binary, str, ft_strlen(str));
-	i = ft_strlen(str) + 4;
+	i = ft_strlen(str);
 	while (i < PROG_NAME_LENGTH)
 	{
 		fta_append_char(binary, 0x00);
@@ -49,13 +54,13 @@ void
 	int i;
 
 	i = 0;
-	while (i < 12)
+	while (i < 8)
 	{
 		fta_append_char(binary, 0x00);
 		i++;
 	}
 	fta_append(binary, str, ft_strlen(str));
-	i = ft_strlen(str) + 12;
+	i = ft_strlen(str) + 8;
 	while (i < COMMENT_LENGTH)
 	{
 		fta_append_char(binary, 0x00);
@@ -64,10 +69,18 @@ void
 }
 
 void
-	cw_append_op(t_array *binary, char *str)
+	cw_append_op(t_array *binary)
 {
-	fta_append_char(&binary, 0x0b);
-	fta_append_char(&binary, 0x68);
+	int i;
+
+	i = 0;
+	while (i < 12)
+	{
+		fta_append_char(binary, 0x00);
+		i++;
+	}
+	fta_append_char(binary, 0x0b);
+	fta_append_char(binary, 0x68);
 }
 
 int
@@ -76,9 +89,10 @@ int
 	t_array	binary;
 
 	binary = NEW_ARRAY(char);
+	cw_append_magic_nbr(&binary);
 	cw_append_name(&binary, "zork");
 	cw_append_comment(&binary, "just a basic living prog");
-	cw_append_op(&binary, char *str);
+	cw_append_op(&binary);
 	cw_print_memory(&binary);
 	return (0);
 }
