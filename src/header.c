6350e4ed459a2b8ae7bddab1e4a9f1e7a8a1d5fa
Rizky Ario <rizkyario@yahoo.com>
2018-04-13 15:46:35 +0200

    Refactored

--- src/header.c
+++ src/header.c
@@ -6,46 +6,11 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 14:56:36 by fpetras           #+#    #+#             */
-/*   Updated: 2018/04/13 15:23:18 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/13 15:42:11 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
-
-int
-	ft_re_matchn(const char* pattern, const char* text, int len)
-{
-	char	*str;
-	int		res;
-
-	res = -1;
-	str = ft_memalloc(len + 1);
-	ft_memcpy(str, text, len);
-	str[len] = '\0';
-	res = ft_re_matchp(ft_re_compile(pattern), str);
-	free(str);
-	return (res);
-}
-
-char
-	*ft_re_match_capture(const char *pattern, const char *group, const char *text)
-{
-	int 	len;
-	int		start;
-	char 	*str;
-
-	if (ft_re_match(pattern, text) == -1)
-		return NULL;
-	start = ft_re_match(group, text);
-	text = text + start;
-	len = ft_strlen(text);
-	while (ft_re_matchn(group, text, len - 1) == 0)
-		len--;
-	str = ft_memalloc(len + 1 - 2);
-	ft_memcpy(str, text + 1, len - 2);
-	str[len] = '\0';
-	return (str);
-}
 
 int
 	ft_get_name(t_asm *a, header_t *h)
