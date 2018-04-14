590d1cb792f2f51df7f3a1986be29729c247cae5
Rizky Ario <rizkyario@yahoo.com>
2018-04-14 10:51:03 +0200

    Added asm_get_paramtype

--- src/operation.c
+++ src/operation.c
@@ -6,11 +6,28 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 15:47:51 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/13 18:21:45 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/14 10:50:41 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
+
+# define PTYPE_REG 0
+# define PTYPE_IND 1
+# define PTYPE_DIR 2
+
+int
+	asm_get_paramtype(char *param)
+{
+	if (ft_re_match("^r\\d+$", param) == 0)
+		return (PTYPE_REG);
+	else if (ft_re_match("^%:\\w+$", param) == 0)
+		return (PTYPE_IND);
+	else if (ft_re_match("^%\\d+$", param) == 0)
+		return (PTYPE_DIR);
+	else
+		return (-1);
+}
 
 int
 	ft_get_op(t_asm *a, header_t *h)
@@ -37,7 +54,13 @@ int
 	int i = 0;
 	while (params[i])
 	{
-		ft_printfln("param %d: %s", i, params[i]);
+		ft_printf("param %d: %s ", i, params[i]);
+		if (asm_get_paramtype(params[i]) == PTYPE_REG)
+			ft_printfln("(Registry)");
+		else if (asm_get_paramtype(params[i]) == PTYPE_IND)
+			ft_printfln("(Indirect)");
+		else if (asm_get_paramtype(params[i]) == PTYPE_DIR)
+			ft_printfln("(Direct)");
 		i++;
 	}
 	ft_printfln("---");
