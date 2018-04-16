bc471cf6928c64df5f20fe93807284dc5d8f5a33
Rizky Ario <rizkyario@yahoo.com>
2018-04-16 22:59:50 +0200

    Handled missused + and -

--- src/asm_parser_param.c
+++ src/asm_parser_param.c
@@ -6,7 +6,7 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 15:47:51 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/16 22:25:30 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/16 22:58:59 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,18 +99,20 @@ int
 		return (T_REG);
 	}
 	else if (ft_re_match("^%:[\\w_\\d]+$", (*param).str) == 0 ||
-			ft_re_match("^%[-+]*\\d+$", (*param).str) == 0)
+			ft_re_match("^%\\d+$", (*param).str) == 0 || ft_re_match("^%-\\d+$", (*param).str) == 0)
 	{
 		(ft_re_match("^%:[\\w_\\d]+$", (*param).str) == 0)? (*param).is_label = 1: 0;
-		(*param).value = asm_get_paramval((*param).str, "[-+]*\\d+");
+		(*param).value = (ft_re_match("^%\\d+$", (*param).str) == 0)?
+		asm_get_paramval((*param).str, "\\d+"): asm_get_paramval((*param).str, "-\\d+");
 		(*param).size = g_op_dict[opcode].d_size;
 		return (T_DIR);
 	}
 	else if (ft_re_match("^:[\\w_\\d]+$", (*param).str) == 0 ||
-			ft_re_match("^[-+]*\\d+$", (*param).str) == 0)
+			ft_re_match("^\\d+$", (*param).str) == 0 || ft_re_match("^%-\\d+$", (*param).str) == 0)
 	{
 		(ft_re_match(":[\\w_\\d]+$", (*param).str) == 0)? (*param).is_label = 1: 0;
-		(*param).value = asm_get_paramval((*param).str, "[-+]*\\d+");
+		(*param).value = (ft_re_match("^%\\d+$", (*param).str) == 0)?
+		asm_get_paramval((*param).str, "\\d+"): asm_get_paramval((*param).str, "-\\d+");
 		(*param).size = 2;
 		return (T_IND);
 	}
