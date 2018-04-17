8a1c02a8f40ed8d526038f2da4c2fefcd0228703
Fabian PETRAS <34797639+fpetras@users.noreply.github.com>
2018-04-17 12:05:43 +0200

    Restructuring

--- include/ft_asm.h
+++ include/ft_asm.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_asm.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 14:00:50 by fpetras           #+#    #+#             */
-/*   Updated: 2018/04/17 08:07:14 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/17 12:00:05 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,6 +128,7 @@ int				ft_is_label(char *line);
 int				ft_get_op(t_asm *a);
 
 int				ft_error(int errcode, int status, char *line);
+int				ft_file_extension(char *file);
 int				ft_file_is_empty(char *file);
 int				ft_line_is_empty(t_asm *a);
 void			ft_skip_empty_lines(t_asm *a);
