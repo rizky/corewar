480b04bb2299e4c42133857798085ddbbdf61001
Fabian PETRAS <34797639+fpetras@users.noreply.github.com>
2018-04-17 12:34:03 +0200

    Fix issue; COMMENT_CHAR in header

--- src/asm_helper_2.c
+++ src/asm_helper_2.c
@@ -6,13 +6,35 @@
 /*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 11:57:53 by fpetras           #+#    #+#             */
-/*   Updated: 2018/04/17 11:58:41 by fpetras          ###   ########.fr       */
+/*   Updated: 2018/04/17 12:28:19 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_asm.h"
 
-void	ft_handle_comments(char **file)
+static int	ft_inside_quotes(char *line, int j)
+{
+	int i;
+	int quotes;
+	int inside;
+
+	i = 0;
+	quotes = 0;
+	inside = 0;
+	while (line[i])
+	{
+		if (line[i] == '\"')
+			quotes++;
+		if (i == j && quotes == 1)
+			inside = 1;
+		i++;
+	}
+	if (quotes == 2 && inside == 1)
+		return (1);
+	return (0);
+}
+
+void		ft_handle_comments(char **file)
 {
 	int i;
 	int j;
@@ -24,14 +46,21 @@ void	ft_handle_comments(char **file)
 		while (file[i][j])
 		{
 			if (file[i][j] == COMMENT_CHAR || file[i][j] == ';')
-				file[i][j] = '\0';
+			{
+				if ((ft_strstr(file[i], NAME_CMD_STRING) ||
+					ft_strstr(file[i], COMMENT_CMD_STRING)) &&
+					ft_inside_quotes(file[i], j))
+					;
+				else
+					file[i][j] = '\0';
+			}
 			j++;
 		}
 		i++;
 	}
 }
 
-void	ft_trim_file(char **file)
+void		ft_trim_file(char **file)
 {
 	int		i;
 	char	*tmp;
