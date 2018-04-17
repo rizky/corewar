179829c10b7a2de3ee483308a818d7090a0194f6
Julianto YEO <jyeo@e1r11p8.42.fr>
2018-04-17 02:16:04 +0200

    error for linebreak in between name or comment

---
+++ resources/errors/name&comment_contain_linebreak.s
@@ -0,0 +1,18 @@
+.name "zork
+
+
+asd"
+.comment "I'M ALIIIIVE
+as"
+
+#r_asm accept name & comment that contain \n in between the string, it will be treated as a whitespace.
+#our_asm didn't accept it
+
+
+lele:
+
+lex:
+
+live	%1
+
+	
