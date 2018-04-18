d3b791109bcb1c8efe3c4472320de0ebfc9534a3
Julianto YEO <jyeo@e1r10p9.42.fr>
2018-04-19 00:32:39 +0200

    new error for label

---
+++ tests/error/label_contain_badchar.s
@@ -0,0 +1,14 @@
+.name "zork"
+.comment "I'M ]ALIIIIVE"
+
+
+#anything other then the defined LABEL_CHARS in op.h should be error
+#LABEL should be always be ended with ':' (ex: lele:, lele should not be accepted)
+
+
+le_12!:
+
+live	%1
+
+liv: live %1
+	
