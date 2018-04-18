1426ff09da92d61cc46fb58d9a70df659e76a705
Julianto YEO <jyeo@e1r10p9.42.fr>
2018-04-19 01:32:12 +0200

    new error

---
+++ tests/error/hash_and_tab_at_EOF.s
@@ -0,0 +1,13 @@
+.name "zork"
+.comment "I'M ]ALIIIIVE"
+
+
+#hash(#) at the end + tab at the end of the file cause error,
+#r_asm does not sees this as error
+
+le_12:
+
+live	%1
+
+liv: live %1	#
+	
