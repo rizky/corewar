c68ac2e8cbef5c7867077f0df1021a2b18e48d95
Fabian <34797639+fpetras@users.noreply.github.com>
2018-04-18 20:56:09 +0200

    Protect malloc

--- src/ft_asm.c
+++ src/ft_asm.c
@@ -6,7 +6,7 @@
 /*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 13:59:42 by fpetras           #+#    #+#             */
-/*   Updated: 2018/04/18 16:39:50 by fpetras          ###   ########.fr       */
+/*   Updated: 2018/04/18 20:53:21 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ static int	ft_read_sourcefile(int i, char **av, t_asm *a)
 	if ((fd = open(av[i], O_RDONLY)) == -1)
 		return (SOURCEFILE);
 	a->path = av[i];
-	file[0] = ft_strnew(0);
+	if ((file[0] = ft_strnew(0)) == NULL)
+		return (MALLOC);
 	while ((ret = read(fd, &buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
@@ -64,8 +65,7 @@ static int	ft_read_sourcefile(int i, char **av, t_asm *a)
 		return (ft_free(file[0], NEWLINE));
 	if ((a->file = ft_strsplit2(file[0], '\n')) == NULL)
 		return (ft_free(file[0], MALLOC));
-	free(file[0]);
-	return (0);
+	return (ft_free(file[0], 0));
 }
 
 static int	ft_init(int ac, char **av, t_asm *a, int i)
