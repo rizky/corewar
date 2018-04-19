1bec019d5d2e1daddb2d5f0436420e94f09c8464
Rizky Ario <rizkyario@yahoo.com>
2018-04-19 13:58:06 +0200

    Fixed freeing unallocated memory

--- src/asm_parser_op.c
+++ src/asm_parser_op.c
@@ -6,7 +6,7 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 15:47:51 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/19 13:25:53 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/19 13:57:43 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,9 +44,9 @@ static int
 	t_param	par;
 
 	param_tab = asm_parser_opparam_tab(line);
+	(*op).param_c = 0;
 	if (asm_wordcounter(line, SEPARATOR_CHAR) > 3)
 		return (ft_free_tab(param_tab, -1));
-	(*op).param_c = 0;
 	while (param_tab && param_tab[(*op).param_c])
 	{
 		if (ft_re_match("^[ \t]*[^\t ]+[ \t]*$", param_tab[(*op).param_c]) == -1)
