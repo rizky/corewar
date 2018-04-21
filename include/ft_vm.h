8ee1e0a4306d36cf80dafca09d1b62497148faae
Fabian PETRAS <34797639+fpetras@users.noreply.github.com>
2018-04-21 14:49:15 +0200

    Norm ft_vm.h

--- include/ft_vm.h
+++ include/ft_vm.h
@@ -6,7 +6,7 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 21:39:11 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/20 15:52:25 by fpetras          ###   ########.fr       */
+/*   Updated: 2018/04/21 14:47:42 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@
 
 typedef struct	s_vm
 {
-		int		dump;
-		int		cycles;
-		char	*players[MAX_PLAYERS + 2];
+	int			dump;
+	int			cycles;
+	char		*players[MAX_PLAYERS + 2];
 }				t_vm;
 
-int		vm_options(char **av, t_vm *v);
-void	vm_print(void);
+int				vm_options(char **av, t_vm *v);
+void			vm_print(void);
 
 #endif
