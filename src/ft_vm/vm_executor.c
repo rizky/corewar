8bcb70bef52209e626e347c34d46e6fddb070ad5
martinouch <martinjozan@gmail.com>
2018-04-24 03:22:46 +0200

    #14

--- src/ft_vm/vm_executor.c
+++ src/ft_vm/vm_executor.c
@@ -13,6 +13,37 @@
 #include "ft_vm.h"
 
 int
+	vm_checker_oc(t_op *op)
+{
+	int param[3];
+	int i;
+
+	if (!g_op_dict[op->opcode].is_oc)
+		return (0);
+	param[0] = (op->oc & 192) >> 6;
+	param[1] = (op->oc & 48) >> 4;
+	param[2] = (op->oc & 12) >> 2;
+	i = 0;
+	while (i != 3)
+	{
+		if (param[i] == IND_CODE)
+			if (!(g_op_dict[op->opcode].p_type[i] & T_IND))
+				return (-1);
+		if (param[i] == DIR_CODE)
+			if (!(g_op_dict[op->opcode].p_type[i] & T_DIR))
+				return (-1);
+		if (param[i] == REG_CODE)
+			if (!(g_op_dict[op->opcode].p_type[i] & T_REG))
+				return (-1);
+		if (param[i] == 0)
+			if (g_op_dict[op->opcode].p_type[i])
+				return (-1);
+		i++;
+	}
+	return (0);
+}
+
+int
 	vm_decompiler_param(t_process *p, t_op *op)
 {
 	int		i;
@@ -21,6 +52,8 @@ int
 	op->param_c = g_op_dict[op->opcode].param_c;
 	while (i < op->param_c)
 	{
+		if (vm_checker_oc(op) == -1)
+			return (-1);
 		op->params[i].type = (g_op_dict[op->opcode].is_oc) ?
 		(op->oc & (0xC0 >> (i * 2))) >> ((3 - i) * 2) : op->oc;
 		(op->params[i].type == REG_CODE) ? op->params[i].size = 1 : 0;
