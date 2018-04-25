71507093b8d5703d3f5907180b82cbd8d4de69cc
rizkyario <rizkyario@yahoo.com>
2018-04-25 18:27:32 +0200

    Fix vm_op_zjmp

--- src/ft_vm/vm_op.c
+++ src/ft_vm/vm_op.c
@@ -6,7 +6,7 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 19:23:14 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/04/25 13:58:45 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/04/25 17:41:14 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,4 +99,6 @@ void
 		value = value % MEM_SIZE;
 	if (g_carrier)
 		p->pc += value - (p->offset + p->pc);
+	else
+		vm_op_inc(vm, p);
 }
--- tests/vm/reg/r1.s
+++ tests/vm/reg/r1.s
@@ -2,6 +2,6 @@
 .comment "just a basic living prog"
 		
 l2:	sti	r1,%:live,%10
-	and	r1,%0,r1
+	and	r1,%0,r2
 live:	live	%1
 	zjmp	%:l2
