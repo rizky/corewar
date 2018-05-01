080ec8a02ee764dc0cf07b88672b1c8e4f81a432
rizkyario <rizkyario@yahoo.com>
2018-05-01 16:07:43 +0200

    Revert vm_sti_print until we have more test cases

--- src/ft_vm/vm_op_print_3.c
+++ src/ft_vm/vm_op_print_3.c
@@ -6,40 +6,11 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 15:13:56 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/05/01 15:07:04 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/05/01 16:06:19 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_vm.h"
-
-void
-	vm_sti_print2(t_process p, int param1, int param2)
-{
-	ft_printf("P %4d | ", p.index);
-	ft_printf("%s", g_op_dict[p.op.opcode].name);
-	ft_printf(" r%d", p.op.params[0].value);
-	(p.op.params[0].type == DIR_CODE) ?
-	ft_printf(" %hd", param1) : ft_printf(" %d", param1);
-	(p.op.params[1].type == DIR_CODE) ?
-	ft_printf(" %hd\n", param2) : ft_printf(" %d\n", param2);
-	if (p.op.params[1].type == DIR_CODE && p.op.params[2].type == DIR_CODE)
-		ft_printf("       | -> store to %d + %d = %d (with pc and mod %d)",
-		param1, param2, (short)param1 + (short)param2,
-		p.offset + p.pc + ((short)param1 + (short)param2) % IDX_MOD);
-	else if (p.op.params[1].type == DIR_CODE)
-		ft_printf("       | -> store to %d + %d = %d (with pc and mod %d)",
-		param1, param2, (short)param1 + param2,
-		p.offset + p.pc + ((short)param1 + param2) % IDX_MOD);
-	else if (p.op.params[2].type == DIR_CODE)
-		ft_printf("       | -> store to %d + %d = %d (with pc and mod %d)",
-		param1, param2, param1 + (short)param2,
-		p.offset + p.pc + (param1 + (short)param2) % IDX_MOD);
-	else
-		ft_printf("       | -> store to %d + %d = %d (with pc and mod %d)",
-		param1, param2, param1 + param2,
-		p.offset + p.pc + (param1 + param2) % IDX_MOD);
-	ft_printf("\n");
-}
 
 void
 	vm_sti_print(t_process p)
@@ -53,14 +24,20 @@ void
 		((p.op.params[2].type == REG_CODE) &&
 			(p.op.params[2].value < 1 || p.op.params[2].value > 16)))
 		return ;
-	param1 = (p.op.params[0].type == REG_CODE) ?
-		g_reg[p.champ][p.op.params[0].value] : p.op.params[0].value;
-	param2 = (p.op.params[1].type == REG_CODE) ?
+	param1 = (p.op.params[1].type == REG_CODE) ?
 		g_reg[p.champ][p.op.params[1].value] : p.op.params[1].value;
-	param1 = (p.op.params[0].type == IND_CODE) ?
-		vm_binary_toint(&g_memory[p.offset + p.pc +
-			p.op.params[0].value % IDX_MOD], 4) : param1;
-	vm_sti_print2(p, param1, param2);
+	param1 = (p.op.params[1].type == IND_CODE) ?
+		vm_binary_toint(&g_memory[p.offset + p.pc + p.op.params[1].value], 4)
+		: param1;
+	param2 = (p.op.params[2].type == REG_CODE) ?
+		g_reg[p.champ][p.op.params[2].value] : p.op.params[2].value;
+	ft_printf("P %4d | ", p.index);
+	ft_printf("%s", g_op_dict[p.op.opcode].name);
+	ft_printfln(" r%d %d %hd", p.op.params[0].value, param1, param2);
+	ft_printf("       | -> store to %d + %d = %d (with pc and mod %d)",
+		param1, param2, param1 + param2,
+		(p.offset + p.pc + param1 + param2) % IDX_MOD);
+	ft_printf("\n");
 }
 
 void
