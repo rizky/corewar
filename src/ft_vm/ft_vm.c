17c33b31b9be9d581b96ef98fbdeccebf2798db2
Rizky Ario <rizkyario@yahoo.com>
2018-05-05 20:48:34 +0200

    Patch segvault for 2 million processes

--- src/ft_vm/ft_vm.c
+++ src/ft_vm/ft_vm.c
@@ -6,7 +6,7 @@
 /*   By: rnugroho <rnugroho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 21:38:33 by rnugroho          #+#    #+#             */
-/*   Updated: 2018/05/05 18:08:49 by rnugroho         ###   ########.fr       */
+/*   Updated: 2018/05/05 20:45:14 by rnugroho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ static void	vm_load_champs(t_vm *vm)
 	vm->winner = vm->champ_size - 1;
 	ft_bzero(&p, sizeof(t_process));
 	vm->process_index = 1;
+	fta_reserve(&(vm->processes), 2147483647 * sizeof(t_process));
 	ft_printfln("Introducing contestants...");
 	while (++i < vm->champ_size)
 	{
