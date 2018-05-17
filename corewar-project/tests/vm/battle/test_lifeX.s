8efc1dfe75962f1775d1e4215681b43b8ba82e35
Rizky Ario <rizkyario@yahoo.com>
2018-05-17 19:49:21 +0200

    Restructur files

---
+++ corewar-project/tests/vm/battle/test_lifeX.s
@@ -0,0 +1,18 @@
+.name "live forever"
+.comment "test aff"
+
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+live %1
+; live %1
+add r2, r3, r4
+zjmp %0
+; label: live %2147483647
+; zjmp %:label
